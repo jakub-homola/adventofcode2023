ffccd2d94462a3d786dc67bca1f78a1fad8b9260
Jakub Homola <jakub329homola@gmail.com>
2023-12-10 08:50:34 +0100

    day9 forgot commit

---
+++ day09/task2.cpp
@@ -0,0 +1,55 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+
+
+int main()
+{
+    std::vector<std::vector<int>> values;
+
+    while(true)
+    {
+        char line[10000];
+        fgets(line, sizeof(line), stdin);
+        if(feof(stdin)) break;
+        values.push_back(std::vector<int>());
+        std::vector<int> & vals = values.back();
+        char * l = line;
+        while(true)
+        {
+            int v;
+            int res = sscanf(l, "%d", &v);
+            vals.push_back(v);
+            while(*l != ' ' && *l != '\0') l++;
+            if(*l == '\0') break;
+            l++;
+        }
+        std::reverse(vals.begin(), vals.end());
+    }
+
+    long long result = 0;
+
+    for(size_t i = 0; i < values.size(); i++)
+    {
+        std::vector<int> & vals = values[i];
+        size_t curr_n = vals.size() - 1;
+        bool is_all_zero = false;
+        while(!is_all_zero)
+        {
+            is_all_zero = true;
+            for(size_t j = 0; j < curr_n; j++)
+            {
+                vals[j] = vals[j+1] - vals[j];
+                if(vals[j] != 0) is_all_zero = false;
+            }
+            curr_n--;
+        }
+
+        for(size_t j = curr_n; j < vals.size(); j++) result += vals[j];
+    }
+
+    printf("%lld\n", result);
+
+    return 0;
+}
